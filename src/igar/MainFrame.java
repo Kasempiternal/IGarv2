cabc6052b565ec757b69f6190353fdedcf4f8da9
izotz <izotz@10.22.0.119>
2018-02-19 10:21:08 +0100

    Initial file

--- src/igar/MainFrame.java
+++ src/igar/MainFrame.java
@@ -63,7 +63,7 @@ public class MainFrame extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         JDialog test = new JDialog();
-        
+           test.setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
